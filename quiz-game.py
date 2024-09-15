325f2fe8a115e20b7cd3be1ea8b7c027b4cc263e
Gedion Abera <gedionable2@gmail.com>
2024-09-15 20:43:49 +0300

    Initial commit: Add quiz-game.py

---
+++ quiz-game.py
@@ -0,0 +1,10 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ")
