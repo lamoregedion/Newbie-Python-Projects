77a1d206ac46ff7dd688bf7d4ba38083136f04ac
Gedion Abera <gedionable2@gmail.com>
2024-09-15 23:48:48 +0300

    Build Choose Your Own Adventure game with interactive options

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,16 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+
+    if answer == "swim":
+        print("You swam acrross and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print('Not a valid option. You lose.')
