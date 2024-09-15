438eea16cd3cca66f29b0c6995dae16c7a7f341f
Gedion Abera <gedionable2@gmail.com>
2024-09-15 23:58:59 +0300

    Develop secure password manager with encryption functionality

--- password_manager.py
+++ password_manager.py
@@ -26,6 +26,7 @@ def view():
             print("User:", user, "| Password:",
                     fer.decrypt(passw.encode()).decode())
 
+
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
@@ -39,3 +40,11 @@ while True:
         "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode == "q":
         break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
