d383df124f9f84c0ba9dac1e91fb227dfc0e9d4b
Gedion Abera <gedionable2@gmail.com>
2024-09-15 23:42:13 +0300

    Add Rock, Paper, Scissors game implementation

---
+++ rock_paper_scissors.py
@@ -0,0 +1,14 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+# options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break        
+
+    if user_input not in ["rock", "paper", "scissors"]:
+        continue
